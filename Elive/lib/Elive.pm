b4e5bef814055c60b49948976ee353f1cc0d1377
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2009-04-22 01:40:32 +0000

    quick bugfix to entity updates :-|
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@71 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive.pm
+++ Elive/lib/Elive.pm
@@ -7,11 +7,11 @@ Elive -  Elluminate Live (c) client library
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Class::Data::Inheritable;
 use base qw{Class::Data::Inheritable};
