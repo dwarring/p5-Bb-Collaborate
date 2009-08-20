6982815b08885473f6bf0012c0692a8d15c587fb
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2009-08-20 22:37:39 +0000

    bumping version to 0.38
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@256 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive.pm
+++ Elive/lib/Elive.pm
@@ -7,11 +7,11 @@ Elive -  Elluminate Live! (c) client library
 
 =head1 VERSION
 
-Version 0.37
+Version 0.38
 
 =cut
 
-our $VERSION = '0.37';
+our $VERSION = '0.38';
 
 use Class::Data::Inheritable;
 use base qw{Class::Data::Inheritable};
