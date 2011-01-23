26de2cbf5cb45caa821f402d1f9896ea8ad718fa
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2011-01-23 19:53:12 +0000

    version 0.79
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@562 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive.pm
+++ Elive/lib/Elive.pm
@@ -7,11 +7,11 @@ Elive - Elluminate Live! (c) SDK bindings
 
 =head1 VERSION
 
-Version 0.79
+Version 0.80
 
 =cut
 
-our $VERSION = '0.79';
+our $VERSION = '0.80';
 
 use Class::Data::Inheritable;
 use base qw{Class::Data::Inheritable};
