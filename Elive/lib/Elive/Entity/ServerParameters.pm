d63d7e9150d7328a5b47da97ec1e2d92547e7d1a
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2011-07-18 23:26:53 +0000

    elive 9.5 support: added aliases for elm 3.0 mispellings
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@820 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive/Entity/ServerParameters.pm
+++ Elive/lib/Elive/Entity/ServerParameters.pm
@@ -72,7 +72,7 @@ has 'serverTelephonyAddress'
 has 'redirectURL' => (is => 'rw', isa => 'Str',
 		      documentation => 'URL to redirect users to after the online session is over.');
 
-## Mispellings - Required to support Elm/3.0 / Elive 9.5.0
+## Mispellings in toolkit - Required to support Elm 3.0 / Elive 9.5.0
 __PACKAGE__->_alias(ModertatorTelephonyAddress => 'ModeratorTelephonyAddress');
 __PACKAGE__->_alias(ModertatorTelephonyPIN => 'ModeratorTelephonyPIN');
 
