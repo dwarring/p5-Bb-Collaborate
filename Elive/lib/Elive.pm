878cf030f66419f421a98e2e3ee7760cd05abf80
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2012-08-08 02:38:07 +0000

    bumped version to 1.28. updated changelog
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@1097 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive.pm
+++ Elive/lib/Elive.pm
@@ -7,11 +7,11 @@ Elive - Elluminate Live! (Blackboard Collaborate) Command Toolkit bindings
 
 =head1 VERSION
 
-Version 1.27
+Version 1.28
 
 =cut
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 use 5.008003;
 
