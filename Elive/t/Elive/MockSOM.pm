8a18b82329b33b445d958fa264749a4e99d89c23
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2012-10-18 20:18:02 +0000

    use base => use parent
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@1122 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/t/Elive/MockSOM.pm
+++ Elive/t/Elive/MockSOM.pm
@@ -113,8 +113,7 @@ sub not_found {
 package t::Elive::MockSOMFault;
 use warnings; use strict;
 
-use Class::Accessor;
-use base qw{Class::Accessor};
+use parent qw{Class::Accessor};
 
 __PACKAGE__->mk_accessors( qw{faultstring result paramsout} );
 
