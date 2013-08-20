635511b2a3b47bdfc316761d2fa4c5c9da2b16b7
dwarring <dwarring@2be58aeb-93cd-45b1-8439-e6c87a8128cd>
2013-08-20 00:12:08 +0000

    removed Elive::Array
    git-svn-id: svn+ssh://svn.code.sf.net/p/elive/code/trunk@1142 2be58aeb-93cd-45b1-8439-e6c87a8128cd

--- Elive/lib/Elive.pm
+++ Elive/lib/Elive.pm
@@ -7,11 +7,11 @@ Elive - Elluminate Live! Manager (ELM) Command Toolkit bindings
 
 =head1 VERSION
 
-Version 1.31
+Version 1.32
 
 =cut
 
-our $VERSION = '1.31';
+our $VERSION = '1.32';
 
 use 5.008003;
 
