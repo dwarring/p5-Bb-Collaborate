a6c56c5769712facde62c5f5fd519e54caa4f866
David Warring <david.warring@gmail.com>
2015-06-30 14:26:56 +1200

    minor changes for 0.02

--- Bb-Collaborate-V3/lib/Bb/Collaborate/V3.pm
+++ Bb-Collaborate-V3/lib/Bb/Collaborate/V3.pm
@@ -15,11 +15,11 @@ Bb::Collaborate::V3 - Perl bindings for the Blackboard Collaborate Standard Brid
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 DESCRIPTION
 
